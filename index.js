ceb08274664db52ac2b40e7e80f62652148a09a2
VINAY KUMAR MAHTO <120046338+Vinay980@users.noreply.github.com>
2025-01-09 00:15:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
-let allBtn = document.querySelectorAll(".drum").length;
-for (let i = 0; i <= allBtn; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    let btnInnerHTML = this.innerHTML;
 
-    switch (btnInnerHTML) {
-      case "w":
-        let tom1 = new Audio("./sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "a":
-        let tom2 = new Audio("./sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "s":
-        let tom3 = new Audio("./sounds/tom-3.mp3");
-        tom3.play();
-        break;
-        case "d":
-        let tom4 = new Audio("./sounds/tom-4.mp3");
-        tom4.play();
-        break;
-        case "j":
-        let snare = new Audio("./sounds/snare.mp3");
-        snare.play();
-        break;
-        case "k":
-        let crash = new Audio("./sounds/crash.mp3");
-        crash.play();
-        break;
-        case "l":
-        let kickBass = new Audio("./sounds/kick-bass.mp3");
-        kickBass.play();
-        break;
-        default: console.log(btnInnerHTML)
-    }
-  });
+// Detecting Button Press
+
+let allBtn = document.querySelectorAll(".drum").length;
+for (var i = 0; i <allBtn; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var btnInnerHTML = this.innerHTML;
+
+    makeSound(btnInnerHTML)
+
+  }
+)}
+
+// Detecting KeyBoard Press
+
+document.addEventListener("keypress",function(event){
+  makeSound(event.key)
+
+})
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+      case "d":
+      let tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+      case "j":
+      let snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+      case "k":
+      let crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+      case "l":
+      let kickBass = new Audio("./sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+      default: console.log(btnInnerHTML)
+  }
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
 }
